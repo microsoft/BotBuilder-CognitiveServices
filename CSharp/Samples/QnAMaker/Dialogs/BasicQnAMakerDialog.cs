d151e45e2b783c01aff883fffeaab79dd7af0bc8
maurok <maurok@gmail.com>
2017-02-22 15:31:11 -0300

    # Adding new dialog to show how to use QnA dialog handler

---
+++ CSharp/Samples/QnAMaker/Dialogs/BasicQnAMakerDialog.cs
@@ -0,0 +1,14 @@
+﻿namespace QnAMakerSampleBot.Dialogs
+{
+    using System;
+    using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+
+    [Serializable]
+    public class BasicQnAMakerDialog : QnAMakerDialog
+    {
+        // Go to https://qnamaker.ai and feed data, train & publish your QnA Knowledgebase.
+        public BasicQnAMakerDialog() : base(new QnAMakerService(new QnAMakerAttribute("set yout subscription key here", "set your kbid here", "I don't understand this right now! Try another query!", 0.50)))
+        {
+        }
+    }
+}

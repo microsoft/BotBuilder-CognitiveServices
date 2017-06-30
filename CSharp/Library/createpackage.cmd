29c2fc4cef06c98e583cd0cfae8fa1d6345ef0c2
Pablo Costantini <pablo.costantini@southworks.com>
2017-06-30 12:39:14 -0300

    Update NuGet spec & createpackage.cmd with LuisActionBinding assembly

--- CSharp/Library/createpackage.cmd
+++ CSharp/Library/createpackage.cmd
@@ -4,8 +4,9 @@ setlocal enabledelayedexpansion
 setlocal enableextensions
 set errorlevel=0
 mkdir ..\nuget
-erase /s ..\nuget\Microsoft.Bot.CognitiveServices.QnAMaker*nupkg
-msbuild /property:Configuration=release Microsoft.Bot.Builder.CognitiveServices.QnAMaker.csproj 
-for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.dll).FileVersionInfo.FileVersion"') do set builder=%%v
-for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.CognitiveServices.QnAMaker.dll).FileVersionInfo.FileVersion"') do set version=%%v
-..\packages\NuGet.CommandLine.3.4.3\tools\NuGet.exe pack Microsoft.Bot.Builder.CognitiveServices.QnAMaker.nuspec -symbols -properties version=%version%;builder=%builder% -OutputDirectory ..\nuget
+erase /s ..\nuget\*.nupkg
+msbuild /property:Configuration=release QnAMaker\Microsoft.Bot.Builder.CognitiveServices.QnAMaker.csproj 
+msbuild /property:Configuration=release LuisActionBinding\Microsoft.Bot.Builder.CognitiveServices.LuisActionBinding.csproj 
+for /f %%v in ('powershell -noprofile "(Get-Command .\QnAMaker\bin\release\Microsoft.Bot.Builder.dll).FileVersionInfo.FileVersion"') do set builder=%%v
+for /f %%v in ('powershell -noprofile "(Get-Command .\QnAMaker\bin\release\Microsoft.Bot.Builder.CognitiveServices.QnAMaker.dll).FileVersionInfo.FileVersion"') do set version=%%v
+..\packages\NuGet.CommandLine.3.4.3\tools\NuGet.exe pack Microsoft.Bot.Builder.CognitiveServices.nuspec -symbols -properties version=%version%;builder=%builder% -OutputDirectory ..\nuget

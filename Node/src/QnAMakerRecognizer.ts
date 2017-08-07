f342b3ac64aff25c494dc59b2d6d931b0e6f7d25
Iván López Santiago <ils@tid.es>
2017-08-07 12:04:04 +0200

    Check HTTP status code, to handle errors (#33)

--- Node/src/QnAMakerRecognizer.ts
+++ Node/src/QnAMakerRecognizer.ts
@@ -113,22 +113,26 @@ export class QnAMakerRecognizer implements builder.IIntentRecognizer {
                     var result: IQnAMakerResults;
                     try {
                         if (!error) {
-                            result = JSON.parse(body);
-                            var answerEntities: builder.IEntity[] = [];
-                            if(result.answers !== null && result.answers.length > 0){
-                                result.answers.forEach((ans) => {
-                                    ans.score /= 100;
-                                    ans.answer = htmlentities.decode(ans.answer);
-                                    var answerEntity = {
-                                        score: ans.score,
-                                        entity: ans.answer,
-                                        type: 'answer'
-                                    }
-                                    answerEntities.push(answerEntity as builder.IEntity);
-                                });
-                                result.score = result.answers[0].score;
-                                result.entities = answerEntities;
-                                result.intent = intentName;
+                            if (response.statusCode === 200) {
+                                result = JSON.parse(body);
+                                var answerEntities: builder.IEntity[] = [];
+                                if(result.answers && result.answers.length > 0){
+                                    result.answers.forEach((ans) => {
+                                        ans.score /= 100;
+                                        ans.answer = htmlentities.decode(ans.answer);
+                                        var answerEntity = {
+                                            score: ans.score,
+                                            entity: ans.answer,
+                                            type: 'answer'
+                                        }
+                                        answerEntities.push(answerEntity as builder.IEntity);
+                                    });
+                                    result.score = result.answers[0].score;
+                                    result.entities = answerEntities;
+                                    result.intent = intentName;
+                                }
+                            } else {
+                                error = new Error(body);
                             }
                         }
                     } catch (e) {

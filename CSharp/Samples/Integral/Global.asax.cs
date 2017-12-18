b4bc9b0003f1083335b48ae93c6748a338431b4a
pablo costantini <pablo.costantini@southworks.com>
2017-12-18 16:29:18 -0300

    Update Bot Samples to use InMemory State Storage

--- CSharp/Samples/Integral/Global.asax.cs
+++ CSharp/Samples/Integral/Global.asax.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Configuration;
+using System.Reflection;
 using System.Web.Http;
 using Autofac;
+using Microsoft.Bot.Builder.Azure;
 using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
 using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
 using Microsoft.Bot.Builder.Internals.Fibers;
+using Microsoft.Bot.Connector;
 
 namespace IntegralSampleBot
 {
@@ -14,7 +18,25 @@ namespace IntegralSampleBot
         {
             Conversation.UpdateContainer(builder =>
             {
+                builder.RegisterModule(new AzureModule(Assembly.GetExecutingAssembly()));
+
+                // Bot Storage: Here we register the state storage for your bot. 
+                // Default store: volatile in-memory store - Only for prototyping!
+                // We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
+                // For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
+                var store = new InMemoryDataStore();
+
+                // Other storage options
+                // var store = new TableBotDataStore("...DataStorageConnectionString..."); // requires Microsoft.BotBuilder.Azure Nuget package 
+                // var store = new DocumentDbBotDataStore("cosmos db uri", "cosmos db key"); // requires Microsoft.BotBuilder.Azure Nuget package 
+
+                builder.Register(c => store)
+                    .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                    .AsSelf()
+                    .SingleInstance();
+
                 builder.RegisterModule(new ReflectionSurrogateModule());
+
                 builder.RegisterModule(new QnAMakerModule(
                     ConfigurationManager.AppSettings["QnAMaker_SubscriptionId"],
                     ConfigurationManager.AppSettings["QnAMaker_KnowledgeBaseId"],
